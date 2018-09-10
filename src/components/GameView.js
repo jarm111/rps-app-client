a4d851c08abfb5bcdbb9284d77ce08bb2b8b25dd
Jarmo Syvälahti <44726773+jarm111@users.noreply.github.com>
2018-09-10 15:32:44 +0300

    Replaced elements in game view with reactstrap components

--- src/components/GameView.js
+++ src/components/GameView.js
@@ -1,29 +1,26 @@
 import React from 'react';
+import { Button, Alert, Badge } from 'reactstrap';
 
 const GameView = props => (
-  <div>
-    <p>BEST STREAK: 10</p>
-    <p>CURRENT STREAK: 5</p>
-    <div>
-      <h1>ROCK-PAPER-SCISSORS</h1>
-      <h3>by jarm111</h3>
-    </div>
-    <div>
-      <div>
-        <button>ROCK</button>
-        <button>PAPER</button>
-        <button>SCISSORS</button>
-      </div>
-      <h2>WIN</h2>
-      <div>
-        <button>ROCK</button>
-        <button>PAPER</button>
-        <button>SCISSORS</button>
+  <div className="container">
+    <div className="row">
+      <div className="col">
+        <Badge color="secondary">ROCK</Badge>
+        <Badge color="info">PAPER</Badge>
+        <Badge color="secondary">SCISSORS</Badge>
       </div>
     </div>
-    <div>
-      <button>RESTART</button>
-      <button>BACK TO MENU</button>
+    <div className="row">
+      <div className="col">
+        <Alert color="success">WIN</Alert>
+      </div>
+    </div>
+    <div className="row">
+      <div className="col">
+        <Button color="primary">ROCK</Button>
+        <Button color="primary">PAPER</Button>
+        <Button color="primary">SCISSORS</Button>
+      </div>
     </div>
   </div>
 );
