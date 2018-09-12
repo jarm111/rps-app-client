2138acdc3565317aca7766cbb854ec889cafb7a1
Jarmo Syvälahti <44726773+jarm111@users.noreply.github.com>
2018-09-12 13:34:27 +0300

    Created game logic for a game round

--- src/components/GameView.js
+++ src/components/GameView.js
@@ -3,12 +3,73 @@ import OpponentRpsIcons from './OpponentRpsIcons';
 import GameStatusText from './GameStatusText';
 import PlayerRpsButtons from './PlayerRpsButtons';
 
-const GameView = props => (
-  <div className="container">
-    <OpponentRpsIcons selected="paper" />
-    <GameStatusText status="tie" />
-    <PlayerRpsButtons />
-  </div>
-);
+class GameView extends React.Component {
+  constructor(props) {
+    super(props);
+    this.getInitialState = this.getInitialState.bind(this);
+    this.state = this.getInitialState();
+    this.handlePlayerRpsButtonClick = this.handlePlayerRpsButtonClick.bind(
+      this
+    );
+    this.drawOpponentSelection = this.drawOpponentSelection.bind(this);
+    this.calculateRoundResult = this.calculateRoundResult.bind(this);
+  }
+
+  getInitialState() {
+    return {
+      opponentSelection: null,
+      playerSelection: null,
+      result: 'choose'
+    };
+  }
+
+  handlePlayerRpsButtonClick(selection) {
+    const opponentSelection = this.drawOpponentSelection();
+    const result = this.calculateRoundResult(selection, opponentSelection);
+    this.setState({
+      playerSelection: selection,
+      opponentSelection: opponentSelection,
+      result: result
+    });
+  }
+
+  drawOpponentSelection() {
+    const rps = ['rock', 'paper', 'scissors'];
+
+    //The maximum is exclusive and the minimum is inclusive
+    const getRandomInt = (min, max) => {
+      min = Math.ceil(min);
+      max = Math.floor(max);
+      return Math.floor(Math.random() * (max - min)) + min;
+    };
+
+    return rps[getRandomInt(0, 3)];
+  }
+
+  calculateRoundResult(playerSelection, opponentSelection) {
+    if (playerSelection === opponentSelection) {
+      return 'tie';
+    }
+    if (
+      (playerSelection === 'scissors' && opponentSelection === 'paper') ||
+      (playerSelection === 'paper' && opponentSelection === 'rock') ||
+      (playerSelection === 'rock' && opponentSelection === 'scissors')
+    ) {
+      return 'win';
+    } else {
+      return 'loss';
+    }
+  }
+
+  render() {
+    return (
+      <div className="container">
+        <OpponentRpsIcons selected={this.state.opponentSelection} />
+        <GameStatusText status={this.state.result} />
+        <PlayerRpsButtons onClick={this.handlePlayerRpsButtonClick} />
+      </div>
+    );
+  }
+}
 
 export default GameView;
--- src/components/PlayerRpsButtons.js
+++ src/components/PlayerRpsButtons.js
@@ -1,14 +1,25 @@
 import React from 'react';
 import { Button } from 'reactstrap';
+import PropTypes from 'prop-types';
 
 const PlayerRpsButtons = props => (
   <div className="row">
     <div className="col">
-      <Button color="primary">ROCK</Button>
-      <Button color="primary">PAPER</Button>
-      <Button color="primary">SCISSORS</Button>
+      <Button color="primary" onClick={() => props.onClick('rock')}>
+        ROCK
+      </Button>
+      <Button color="primary" onClick={() => props.onClick('paper')}>
+        PAPER
+      </Button>
+      <Button color="primary" onClick={() => props.onClick('scissors')}>
+        SCISSORS
+      </Button>
     </div>
   </div>
 );
+
+PlayerRpsButtons.propTypes = {
+  onClick: PropTypes.func
+};
 
 export default PlayerRpsButtons;
