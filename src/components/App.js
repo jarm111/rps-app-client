7fb91e6c2cda6f2aa769d5eaab1ec152f8741fd7
Jarmo Syvälahti <44726773+jarm111@users.noreply.github.com>
2018-09-21 11:04:53 +0300

    Converted App to functional component

--- src/components/App.js
+++ src/components/App.js
@@ -1,18 +1,14 @@
-import React, { Component } from 'react';
+import React from 'react';
 import ConnectedGameView from '../containers/ConnectedGameView';
 import './App.css';
 
-class App extends Component {
-  render() {
-    return (
-      <div className="App">
-        <header className="App-header">
-          <h1 className="App-title">Welcome to RPM</h1>
-        </header>
-        <ConnectedGameView />
-      </div>
-    );
-  }
-}
+const App = props => (
+  <div className="App">
+    <header className="App-header">
+      <h1 className="App-title">Welcome to RPM</h1>
+    </header>
+    <ConnectedGameView />
+  </div>
+);
 
 export default App;
