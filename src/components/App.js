18c45cf3cec765dd5394e6e31b818d16c3ed8964
Jarmo Syvälahti <44726773+jarm111@users.noreply.github.com>
2018-09-24 10:40:08 +0300

    Created NavigationBar component

--- src/components/App.js
+++ src/components/App.js
@@ -1,9 +1,11 @@
 import React from 'react';
 import ConnectedGameView from '../containers/ConnectedGameView';
+import NavigationBar from './NavigationBar';
 import './App.css';
 
 const App = props => (
   <div className="App">
+    <NavigationBar />
     <header className="App-header">
       <h1 className="App-title">Welcome to RPM</h1>
     </header>
---
+++ src/components/NavigationBar.js
@@ -0,0 +1,53 @@
+import React from 'react';
+import {
+  Collapse,
+  Navbar,
+  NavbarToggler,
+  NavbarBrand,
+  Nav,
+  NavItem,
+  NavLink
+} from 'reactstrap';
+
+class NavigationBar extends React.Component {
+  constructor(props) {
+    super(props);
+
+    this.toggle = this.toggle.bind(this);
+    this.state = {
+      isOpen: false
+    };
+  }
+
+  toggle() {
+    this.setState({
+      isOpen: !this.state.isOpen
+    });
+  }
+
+  render() {
+    return (
+      <div>
+        <Navbar color="light" light expand="md">
+          <NavbarBrand href="/">RPS</NavbarBrand>
+          <NavbarToggler onClick={this.toggle} />
+          <Collapse isOpen={this.state.isOpen} navbar>
+            <Nav className="ml-auto" navbar>
+              <NavItem>
+                <NavLink href="/home/">HOME</NavLink>
+              </NavItem>
+              <NavItem>
+                <NavLink href="/play/">PLAY</NavLink>
+              </NavItem>
+              <NavItem>
+                <NavLink href="/login/">LOGIN</NavLink>
+              </NavItem>
+            </Nav>
+          </Collapse>
+        </Navbar>
+      </div>
+    );
+  }
+}
+
+export default NavigationBar;
