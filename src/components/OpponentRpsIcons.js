3820d43f03e95732409cca31aaa3b1ebb1cb6dd0
Jarmo Syvälahti <44726773+jarm111@users.noreply.github.com>
2018-09-26 14:46:20 +0300

    Added rock, paper, scissors images to OpponentRpsIcons

--- src/components/OpponentRpsIcons.js
+++ src/components/OpponentRpsIcons.js
@@ -2,6 +2,10 @@ import React from 'react';
 import { Col, Badge } from 'reactstrap';
 import PropTypes from 'prop-types';
 import { Rps } from '../model/enums';
+import rock from '../images/rock.svg';
+import paper from '../images/paper.svg';
+import scissors from '../images/scissors.svg';
+import './OpponentRpsIcons.css';
 
 const OpponentRpsIcons = props => {
   const selectedColor = 'info';
@@ -9,18 +13,23 @@ const OpponentRpsIcons = props => {
 
   return (
     <Col>
-      <Badge color={props.selected === Rps.Rock ? selectedColor : defaultColor}>
-        ROCK
+      <Badge
+        color={props.selected === Rps.Rock ? selectedColor : defaultColor}
+        className="OpponentRpsIcons-badge"
+      >
+        <img src={rock} className="OpponentRpsIcons-img" alt="rock" />
       </Badge>
       <Badge
         color={props.selected === Rps.Paper ? selectedColor : defaultColor}
+        className="OpponentRpsIcons-badge"
       >
-        PAPER
+        <img src={paper} className="OpponentRpsIcons-img" alt="paper" />
       </Badge>
       <Badge
         color={props.selected === Rps.Scissors ? selectedColor : defaultColor}
+        className="OpponentRpsIcons-badge"
       >
-        SCISSORS
+        <img src={scissors} className="OpponentRpsIcons-img" alt="scissors" />
       </Badge>
     </Col>
   );
