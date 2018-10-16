1ee09212cc6dc1aea6fcb1df3f973d1ec5e99fe0
Jarmo Syvälahti <44726773+jarm111@users.noreply.github.com>
2018-10-16 11:15:34 +0300

    Toggle login/logout button

--- src/components/LoginView.js
+++ src/components/LoginView.js
@@ -3,30 +3,38 @@ import PropTypes from 'prop-types';
 import { Col, Container, Row } from 'reactstrap';
 import { GoogleLogin, GoogleLogout } from 'react-google-login';
 
-const LoginView = props => (
-  <Container>
-    <Row>
-      <Col>
-        <GoogleLogin
-          clientId={process.env.REACT_APP_GOOGLE_CLIENT_ID}
-          buttonText="Google Login"
-          onSuccess={props.handleGoogleLoginSuccess}
-          onFailure={props.handleGoogleLoginFailure}
-        />
-      </Col>
-    </Row>
-    <Row>
-      <Col>
-        <GoogleLogout
-          buttonText="Google Logout"
-          onLogoutSuccess={props.handleGoogleLogoutSuccess}
-        />
-      </Col>
-    </Row>
-  </Container>
-);
+const LoginView = props => {
+  let button;
+
+  if (!props.isLoggedIn) {
+    button = (
+      <GoogleLogin
+        clientId={process.env.REACT_APP_GOOGLE_CLIENT_ID}
+        buttonText="Google Login"
+        onSuccess={props.handleGoogleLoginSuccess}
+        onFailure={props.handleGoogleLoginFailure}
+      />
+    );
+  } else {
+    button = (
+      <GoogleLogout
+        buttonText="Google Logout"
+        onLogoutSuccess={props.handleGoogleLogoutSuccess}
+      />
+    );
+  }
+
+  return (
+    <Container>
+      <Row>
+        <Col>{button}</Col>
+      </Row>
+    </Container>
+  );
+};
 
 LoginView.propTypes = {
+  isLoggedIn: PropTypes.bool.isRequired,
   handleGoogleLoginSuccess: PropTypes.func.isRequired,
   handleGoogleLoginFailure: PropTypes.func.isRequired,
   handleGoogleLogoutSuccess: PropTypes.func.isRequired
