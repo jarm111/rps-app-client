e87d4086e89cb9847f00b3d212b430f40bae11db
Jarmo Syvälahti <44726773+jarm111@users.noreply.github.com>
2018-10-23 12:31:48 +0300

    Sign-in button style follows Google's Sign-In Branding Guidelines

--- src/components/LoginView.js
+++ src/components/LoginView.js
@@ -3,12 +3,14 @@ import PropTypes from 'prop-types';
 import { Col, Container, Row } from 'reactstrap';
 import { GoogleLogin, GoogleLogout } from 'react-google-login';
 import { GOOGLE_CLIENT_ID } from '../utils/config';
+import './LoginView.css';
 
 const LoginView = props => {
   const loginButton = (
     <GoogleLogin
+      className="LoginView-GoogleLogin"
       clientId={GOOGLE_CLIENT_ID}
-      buttonText="Google Sign-in"
+      buttonText=""
       onSuccess={response =>
         props.handleGoogleLoginSuccess(response, props.history)
       }
