85defe7eb5c88a43ac39a9567be1366e5123756a
Jarmo Syvälahti <44726773+jarm111@users.noreply.github.com>
2018-11-11 12:25:44 +0200

    Create 404 page

---
+++ src/components/NoMatch.js
@@ -0,0 +1,15 @@
+import React from 'react';
+import { Col, Container, Row } from 'reactstrap';
+
+const NoMatch = () => (
+  <Container className="mb-4">
+    <Row>
+      <Col>
+        <h1>404</h1>
+        <h2>Oops! Page not found.</h2>
+      </Col>
+    </Row>
+  </Container>
+);
+
+export default NoMatch;
